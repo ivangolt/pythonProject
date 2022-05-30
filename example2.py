a42f46977b18138d2fcd44178d8d8564d07edbd8
ivangolt <102701882+ivangolt@users.noreply.github.com>
2022-05-30 21:55:49 +0300

    initial commit

--- example2.py
+++ example2.py
@@ -1,10 +1,16 @@
-words = input().split()
 
-def smash(words):
-    return " ".join(words)
+downloading videos from youtube in python 3.x for the reference you can check https://python-pytube.readthedocs.io/en/latest/user/quickstart.html#downloading-a-video
 
-print(smash(words))
+from pytube import YouTube
+import os
 
+def downloadYouTube(videourl, path):
 
+    yt = YouTube(videourl)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    yt.download(path)
 
+downloadYouTube('https://www.youtube.com/watch?v=zNyYDHCg06c', './videos/FindingNemo1')
 
